2f635f9b69d928a1e800e1b3442e2497e4bed60b
stidjion <khanrehanasif10987654321@gmail.com>
2025-12-06 21:14:46 +0530

    create sheet and switch sheet functions added

--- connectors/excel_connector.py
+++ connectors/excel_connector.py
@@ -1,6 +1,6 @@
 import openpyxl as px
 from openpyxl import load_workbook
-import pandas
+import pandas as pd
 
 class ExcelConnector:
     def __init__(self, file_path):
@@ -21,14 +21,35 @@ class ExcelConnector:
          return wb
     def _load_dataframe(self):
         try:
-            df = pandas.read_excel(self.file_path)
+            df = pd.read_excel(self.file_path, sheet_name=self.ws.title)
             return df
         except:
-            df = pandas.DataFrame()
+            df = pd.DataFrame()
             return df
-    def save_dataframe(self, df):
-        df.to_excel(self.file_path, index=False)
+    def save_dataframe(self):
+        with pd.ExcelWriter(self.file_path, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
+          self.df.to_excel(writer, sheet_name=self.ws.title, index=False)
+
         return True
+    def set_active_sheet(self, sheet_name):
+        if sheet_name in self.wb.sheetnames:
+            self.ws = self.wb[sheet_name]
+            self.df = self._load_dataframe()
+            return True
+        else:
+                print(f"Sheet '{sheet_name}' does not exist.")
+                return False
+    def create_sheet(self, sheet_name):
+        try:
+            self.wb.create_sheet(title=sheet_name)
+            self.wb.save(self.file_path)
+            self.ws = self.wb[sheet_name]
+            self.df = pd.DataFrame()
+            return True
+        except:
+            print(f"Failed to create sheet '{sheet_name}'.")
+            return False
+
     
 
 
